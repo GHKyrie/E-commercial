365f43fab96b2b99b9c2fbb1eef9d9ff96101825
Κυριε ελεησον <nemasterya@gmail.com>
2021-03-22 23:43:59 +0500

    Routing
    Began configuting routing.
    Now clicking the SSD component makes browser respond and go to the corresponding page

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,29 +12,34 @@ class Directory extends React.Component {
                     {
                         title: 'SSD',
                         imageUrl: 'https://i.ibb.co/z4Tg37B/ssd.jpg',
-                        id: 1
+                        id: 1,
+                        linkUrl: 'ssd'
                     },
                     {
                         title: 'ПРОЦЕССОРЫ',
                         imageUrl: 'https://i.ibb.co/JsCngWy/CPU.jpg',
-                        id: 2
+                        id: 2,
+                        linkUrl: ''
                     },
                     {
                         title: 'МАТЕРИНСКИЕ ПЛАТЫ',
                         imageUrl: 'https://i.ibb.co/zZdx8n5/mat.jpg',
-                        id: 3
+                        id: 3,
+                        linkUrl: ''
                     },
                     {
                         title: 'КОМПЛЕКТУЮЩИЕ',
                         imageUrl: 'https://i.ibb.co/zQhcY5W/parts.jpg',
                         size: 'large',
-                        id: 4
+                        id: 4,
+                        linkUrl: ''
                     },
                     {
                         title: 'СБОРКИ',
                         imageUrl: 'https://i.ibb.co/vwpttRJ/sborka.jpg',
                         size: 'large',
-                        id: 5
+                        id: 5,
+                        linkUrl: ''
                     }
                 ]
         }
@@ -44,8 +49,8 @@ class Directory extends React.Component {
         return (
             <div className="directory-menu">
                 {
-                    this.state.sections.map( ({ title, imageUrl, size, id }) => (
-                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                    this.state.sections.map( ({ id, ...otherSectionProps }) => (
+                        <MenuItem key={id} {...otherSectionProps} />
                     ))
                 }
             </div>

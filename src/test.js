478b00ca033ddc7af09eb2cce9719cc00d798da6
Κυριε ελεησον <45338149+GHKyrie@users.noreply.github.com>
2021-04-15 23:36:35 +0500

    Firebase database
    Firebase database implementation started

---
+++ src/test.js
@@ -0,0 +1,8 @@
+import firebase from "firebase";
+import 'firebase/firestore';
+
+const firestore = firebase.firestore();
+
+firestore.collection('users').doc('VGSzbbdT7a201opKbX0d').collection('cartItems').doc('61o97fiuCCEMlrVasqCK');
+firestore.doc('/users/VGSzbbdT7a201opKbX0d/cartItems/61o97fiuCCEMlrVasqCK');
+firestore.collection('/users/VGSzbbdT7a201opKbX0d/cartItems');
